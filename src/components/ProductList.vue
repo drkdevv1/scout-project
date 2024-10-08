4db0bcf8f53ef99892fa304e277fc60101e5d76e
Jose Calderon <josephwantri@gmail.com>
2024-10-07 19:21:09 -0500

    feat(styles): add new styles for ProductList using Tailwind

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,5 +1,6 @@
 <script>
 import ProductCard from "@/components/ProductCard.vue";
+
 export default {
   components: {
     ProductCard,
@@ -11,12 +12,14 @@ export default {
 </script>
 
 <template>
-  <div>
-    <ProductCard
-        v-for="(product, index) in products"
-        :key="index"
-        :product="product"
-    />
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+      <ProductCard
+          v-for="(product, index) in products"
+          :key="index"
+          :product="product"
+      />
+    </div>
   </div>
 </template>
 
